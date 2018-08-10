27ca0f9590402203ca460ff3cff9be8d047743c5
w.lytovka <w.lytovka@koksztys.pl>
2018-08-10 09:19:30 +0200

    few little changes

--- mkdir_from_list.py
+++ mkdir_from_list.py
@@ -3,11 +3,9 @@ import re
 def main():
 	f = open("list.txt","r")
 	f1 = f.readlines()
-#	f1 = f1.rstrip('\n')
 	f1 = [(el.strip()) for el in f1]
 	f1[:] = [item for item in f1 if item != '']	
-	print(f1)
 	for line in f1:
-		os.makedirs(line, exist_ok=True)
+		os.makedirs("../"+line, exist_ok=True)
 	f.close()
 main()
